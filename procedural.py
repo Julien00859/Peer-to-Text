4e1daf24afc58fe946faf4213c67172e2176ddf9
Julien00859 <Julien00859@gmail.com>
2015-07-13 12:55:36 +0200

    authentification codé, le thread server a été placé dans un autre fichier ajout d'un petit shell dans le main loop

---
+++ procedural.py
@@ -0,0 +1,27 @@
+from profile import *
+from server import *
+import json
+import os
+
+profiles = list()
+for p in os.listdir(os.getcwd()+"/profiles/"):
+	profiles.append(p)
+	print("{:02d}. {}\t{}".format(len(profiles), p[p.find("_")+1:], p[:p.find("_")]))
+moi = PrivateProfile(profiles[int(input("\nSelect your profile: "))-1])
+
+server = server(moi)
+server.start()
+
+print("\nPython Shell - Type 'stop' to stop all the process.")
+while True:
+	try:
+		msg = input(">>> ")
+		if msg=="stop":
+			break
+		eval(msg)
+	except Exception as ex:
+		print(ex)
+
+print("Stopping...")
+server.stop()
+print("Stopped")

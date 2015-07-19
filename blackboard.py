8affd83c40d0f0876a6a2d0cd8dd981dd3fc7884
Julien008 <Julien00859@gmail.com>
2015-07-19 23:20:01 +0200

    ajout de la fonction print(blackboard())

--- blackboard.py
+++ blackboard.py
@@ -1,13 +1,13 @@
 class blackboard():
     #0 Salut à tous\n
     #1 Je m'appelle\n
-    #2 Julien\n
-    #  012345678901\n
+    #2 Julien
+    #  0123456789012
 
     def __init__(self):
         self.blackboard = [[]]
     
-    def write(pos, msg):
+    def write(self, pos, msg):
         for c in msg:
             if c == "\n":
                 self.blackboard[pos[0]].insert(pos[1], c)
@@ -17,7 +17,7 @@ class blackboard():
                 self.blackboard[pos[0]].insert(pos[1], c)
                 pos[1]+=1
 
-    def erase(pos, length):
+    def erase(self, pos, length):
         while length > 0
             if pos[1] == 0 and length >= len(blackboard[pos[0]]):
                 del blackboard[pos[0]]
@@ -30,3 +30,9 @@ class blackboard():
             else:
                 del blackboard[pos[0]][pos[1], pos[1]+length]
                 length=0
+
+    def __str__(self):
+        s = str()
+        for x in range(len(blackboard)):
+            s+="".join(self.blackboard[x])
+        return s
